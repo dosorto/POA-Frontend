0b0a9abb641a71f077d8d50471f108bd94420dd3
RaulinhoDev2 <113661523+RaulinhoDev2@users.noreply.github.com>
2022-09-23 00:05:12 -0600

    configuracion de servicios, componentes y interfac

--- POA/src/app/app.module.ts
+++ POA/src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { UserroleComponent } from './components/userrole/userrole.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { objToArrayPipe } from './objToArray.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserroleComponent,
+    objToArrayPipe
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatCardModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
