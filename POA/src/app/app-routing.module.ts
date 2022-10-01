5b18c16d8f87ff2b9f4db93949cf7257b26634f4
LeticiaOchoa <88337103+LeticiaOchoa@users.noreply.github.com>
2022-09-30 19:24:47 -0600

    Squashed commit of the following:
    commit 45abf49ee6cefa1fa4886f0be34a4e87251a1cc3
    Author: RaulinhoDev2 <113661523+RaulinhoDev2@users.noreply.github.com>
    Date:   Fri Sep 30 16:57:48 2022 -0600
    
        Cambios en el front de PEI
    
    commit 89842b3cd8f4042567184a5970994079044610a7
    Author: RaulinhoDev2 <113661523+RaulinhoDev2@users.noreply.github.com>
    Date:   Wed Sep 28 21:43:35 2022 -0600
    
        interfaz y funcionamiento del crear PEI
    
    commit 3e075d335a43288ed46889fa7915e975d6f0d7d6
    Author: RaulinhoDev2 <113661523+RaulinhoDev2@users.noreply.github.com>
    Date:   Mon Sep 26 23:12:25 2022 -0600
    
        creación componentes, servicios e interfaz PEI
    
    commit 0b0a9abb641a71f077d8d50471f108bd94420dd3
    Author: RaulinhoDev2 <113661523+RaulinhoDev2@users.noreply.github.com>
    Date:   Fri Sep 23 00:05:12 2022 -0600
    
        configuracion de servicios, componentes y interfac
    
    commit cf80aa13ad940a3cef6433005e43d1f06333b89d
    Author: RaulinhoDev2 <113661523+RaulinhoDev2@users.noreply.github.com>
    Date:   Thu Sep 22 23:53:10 2022 -0600
    
        Dise;o del front
    
    commit 2db1779664664d0efe94af69517579b0f0e63045
    Author: RaulinhoDev2 <raul.quiroz@unah.hn>
    Date:   Sun Sep 18 16:38:00 2022 -0600
    
        Correcciones en el formulario de la interfaz
    
    commit ec9b816f934ab54cdce27e318e3ff1e1d91fa6f8
    Author: RaulinhoDev2 <raul.quiroz@unah.hn>
    Date:   Sun Sep 18 16:06:29 2022 -0600
    
        Se agregaron los diseños css para la interfaz

--- POA/src/app/app-routing.module.ts
+++ POA/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InsertPEIComponent } from './components/new-pei/new-pei.component';
+import { DisablePEIComponent } from './components/disable-pei/disable-pei.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'insertPEI', component: InsertPEIComponent},
+  {path: 'disablePEI', component: DisablePEIComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- POA/src/app/app.module.ts
+++ POA/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
+import { MatTableModule } from '@angular/material/table';
+import { RouterModule, Routes } from '@angular/router';
+import { InsertPEIComponent } from './components/new-pei/new-pei.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListPermisosComponent } from './components/list-permisos/list-permisos.component';
@@ -11,6 +17,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { ListPEIComponent } from './components/list-pei/list-pei.component';
 import { AllAgregarActualizarDesactivarPeiComponent } from './components/all-agregar-actualizar-desactivar-pei/all-agregar-actualizar-desactivar-pei.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { UserroleComponent } from './components/userrole/userrole.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { objToArrayPipe } from './objToArray.pipe';
+import { DisablePEIComponent } from './components/disable-pei/disable-pei.component';
 
 
 @NgModule({
@@ -19,13 +32,26 @@ import { AllAgregarActualizarDesactivarPeiComponent } from './components/all-agr
     ListPermisosComponent,
     AgregarEditarPermisoComponent,
     ListPEIComponent,
-    AllAgregarActualizarDesactivarPeiComponent
+    AllAgregarActualizarDesactivarPeiComponent,
+    UserroleComponent,
+    objToArrayPipe,
+    InsertPEIComponent,
+    DisablePEIComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    NoopAnimationsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatCardModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatPaginatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
