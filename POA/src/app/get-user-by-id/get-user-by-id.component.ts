7ee7a2156e94713522e4ade39ec1072ede83f18c
jaimeluna24 <jaimeluna600@gmail.com>
2022-10-03 00:23:21 -0600

    SYSCURLP-5/ actualizacion de componente para obtener usuario por id

--- POA/src/app/get-user-by-id/get-user-by-id.component.ts
+++ POA/src/app/get-user-by-id/get-user-by-id.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { UsuarioService} from '../SERVICES/usuario.service';
+import { UsuarioService} from './usuario.service';
 //import { AlertService } from '../SERVICES/alert.service';
 
 
