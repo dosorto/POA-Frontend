90b20963c0649b026af91d9db818cd7d2880fd17
onielos <onielosespinal@gmail.com>
2022-10-04 12:43:08 -0600

    merge eliminar objetivos

--- POA/src/app/objetivos/objetivos.component.ts
+++ POA/src/app/objetivos/objetivos.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ObjetivosService } from './objetivos.service';
 import { ToastrService } from 'ngx-toastr';
-import { FormGroup, FormControl, Validators, AbstractControl,ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl,ReactiveFormsModule, SelectControlValueAccessor } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { objetivo } from './objetivos.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-objetivos',
@@ -11,8 +12,8 @@ import { objetivo } from './objetivos.model';
   styleUrls: ['./objetivos.component.css']
 })
 export class ObjetivosComponent implements OnInit {
-  
-  public objetivo:FormGroup = new FormGroup({
+  _delete:any;
+    public objetivo:FormGroup = new FormGroup({
     nombre: new FormControl('',[Validators.required]),
     idDimension: new FormControl('',[Validators.required]),
     idPei: new FormControl('',[Validators.required])
@@ -44,23 +45,56 @@ export class ObjetivosComponent implements OnInit {
     this.objetivosList = response.allObjetivo);
     
     }
-
-
-  eliminarObjetivo(id: any) {
-    this.objetivosService.eliminarObjetivo(id).subscribe((response:any) => {
-      this.toastr.error('El producto fue eliminado con exito' ,'Producto Eliminado');
-      this.mostrarObjetivo();
-    }, error => {
-      console.log(error);
-    })
-  }
+    set_id_delete(id:Number){
+      this._delete = id;
+      console.log(this._delete)
+    }
+    async delete() {
+      
+      await this.objetivosService.eliminarObjetivo(this._delete)
+      Swal.fire({
+        title: 'Eliminado con exito',
+        showConfirmButton: false,
+        color:'white',
+        background:'#F5B7B1',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+        
+      })
+      setTimeout(function() {
+        window.location.reload();
+      },3000);
+      
+        
+      window.location.reload();
+      
+    }
+  // eliminarObjetivo(id: any) {
+  //   this.objetivosService.eliminarObjetivo(id).subscribe((response:any) => {
+  //     this.toastr.error('El producto fue eliminado con exito' ,'Producto Eliminado');
+  //     this.mostrarObjetivo();
+  //   }, error => {
+  //     console.log(error);
+  //   })
+  // }
 // Función para obtener los datos del formulario y almacenarlos.
   postObjetivo(form:objetivo):any {
     this.objetivosService.postObjetivo(form).subscribe(data=>{
       console.log(data);
-      window.location.reload();
-      this.objetivo.reset();
-    },(error:any)=>{
+      Swal.fire({
+        icon: 'success',
+        title: '¡Objetivo Registrado!',
+        showConfirmButton: false,
+        timer: 2500
+      })
+      setTimeout(function() {
+        window.location.reload();
+      },2500);
+    }, (error:any)=>{
       })
   }
 
