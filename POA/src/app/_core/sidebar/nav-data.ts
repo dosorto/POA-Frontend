470c629c29786a0015cf8cdf9a584a6d43962967
alejandra Fortin <88343738+alejandrafortin@users.noreply.github.com>
2022-10-08 11:34:29 -0600

    pull

---
+++ POA/src/app/_core/sidebar/nav-data.ts
@@ -0,0 +1,7 @@
+export const navbarData = [
+    {
+        routeLink:'home',
+        icon: 'fal fa-home',
+        label: 'Menú Principal'
+    }
+];
---
+++ POA/src/app/_core/top-bar/top-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Storage } from '../global-services/local_storage.service';
+
+@Component({
+  selector: 'top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css']
+})
+export class TopBarComponent implements OnInit {
+  user = this.local.get_storage("user");
+  constructor(private local:Storage) { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
