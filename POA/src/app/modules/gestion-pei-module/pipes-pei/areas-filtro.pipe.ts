4c3d49beb472292d147dbca1cf30379209a86316
CRIJOSAOR <88340482+CRIJOSAOR@users.noreply.github.com>
2022-10-29 13:14:10 -0600

    filtro y servicios

--- POA/src/app/modules/gestion-pei-module/pipes-pei/areas-filtro.pipe.ts
+++ POA/src/app/modules/gestion-pei-module/pipes-pei/areas-filtro.pipe.ts
@@ -1,12 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Area }from "../interfaces-pei/area.model";
+import { Pei } from "../interfaces-pei/pei.model";
+import { Objetivo } from "../interfaces-pei/objetivo.model";
+import { Dimension } from "../interfaces-pei/dimension.model";
 
 @Pipe({
   name: 'areasFiltro'
 })
 export class AreasFiltroPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(Area: Array<Area>, busqueda:string): Array<Area> {
+    const result:Array<Area> = [];
+    for(const post of Area){
+      if((post.nombre.toLocaleLowerCase().includes(busqueda.toLowerCase())) || (post.nombre.toUpperCase().includes(busqueda.toUpperCase()))){
+         result.push(post);
+      };
+    };
+    return result;
   }
 
 }
