3a01204966c11952ee59612ee9649101bb8a2937
CRIJOSAOR <88340482+CRIJOSAOR@users.noreply.github.com>
2022-10-30 10:14:32 -0600

    pantalla listar

--- POA/src/app/modules/gestion-pei-module/components-pei/areas/all-area-component/all-area-component.component.ts
+++ POA/src/app/modules/gestion-pei-module/components-pei/areas/all-area-component/all-area-component.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Storage } from 'src/app/_core/global-services/local_storage.service';
-import { AreaService } from 'src/app/archivos_antiguos/area/area.service';
+import { AreasService } from '../../../services-pei/areas.service';
 import { Area } from '../../../interfaces-pei/area.model';
 import { Pei } from "../../../interfaces-pei/pei.model";
 import { Objetivo } from "../../../interfaces-pei/objetivo.model";
@@ -18,7 +18,7 @@ import Swal from 'sweetalert2';
 })
 export class AllAreaComponentComponent implements OnInit {
   area:Array<Area>=[];
-  listaObjetivos: Array<Objetivo>=[];
+  listaObjetivos: Array<Objetivo >=[];
   user = this.Storage.get_storage("user");
   filter:string=""; // para filtar la tabla
   pei_seleccionado:string="";
@@ -33,22 +33,21 @@ export class AllAreaComponentComponent implements OnInit {
 
   constructor(
     private Storage:Storage, 
-              private service:AreaService,
+              private service:AreasService,
               private router:Router
   ) { }
 
   ngOnInit(): void {
     this.initData();
-  }
-  async initData(){
+  }async initData(){
     let area = await firstValueFrom(this.service.getArea())
-   //this.area = area;
+    this.area = area;
     this.maxPages = Math.round(this.area.length / this.step ) + 1  // cantidad de paginas para los botones
     if((this.area.length % this.step ) !== 0 ){this.maxPages++}; // si sobran pocos elementos agrega otra pagina
     this.enumPages =  Array(this.maxPages).fill(null).map((x,i)=>i).slice(1,this.maxPages);
     console.log(this.area.length);
     const Objetivos = await firstValueFrom(this.service.getObjetivos());
-   // this.listaObjetivos = Objetivos;
+    this.listaObjetivos = Objetivos;
     console.log(this.listaObjetivos)
   }
 
