fab27ce6fcba317ebf06bd1fae9d7d63bf973cfe
onielos <onielosespinal@gmail.com>
2022-10-29 22:57:30 -0600

    paginacion objetivos

--- POA/src/app/modules/gestion-pei-module/interfaces-pei/objetivo.model.ts
+++ POA/src/app/modules/gestion-pei-module/interfaces-pei/objetivo.model.ts
@@ -1,5 +1,5 @@
-// import { Pei } from "./pei.model";
-// import { Dimension } from './dimension.model';
+ import { Pei } from "./pei.model";
+ import { Dimension } from './dimension.model';
 export interface Objetivo {
     id:          number;
     nombre:      string;
@@ -12,24 +12,5 @@ export interface Objetivo {
     pei:         Pei;
     dimension:   Dimension;
 }
-export interface Dimension {
-    id:          number;
-    nombre:      string;
-    descripcion: string;
-    isDelete:    boolean;
-    createdAt:   Date;
-    updatedAt:   Date;
-    idPei:       number;
-}
 
-export interface Pei {
-    id:          number;
-    name:        string;
-    initialYear: Date;
-    finalYear:   Date;
-    isActive:    boolean;
-    isDelete:    boolean;
-    createdAt:   Date;
-    updatedAt:   Date;
-}
 
