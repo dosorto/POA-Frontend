de2c096948c78fc9d70c8b92a31dda84f6c02008
CRIJOSAOR <88340482+CRIJOSAOR@users.noreply.github.com>
2022-11-01 11:54:39 -0600

    Revert "Squashed commit of the following:"
    This reverts commit 02786c0fe6081ac52131493a0d895967957ba2bd.

--- POA/src/app/modules/gestion-pei-module/pipes-pei/areas-filtro.pipe.ts
+++ POA/src/app/modules/gestion-pei-module/pipes-pei/areas-filtro.pipe.ts
@@ -1,19 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Area }from "../interfaces-pei/area.model";
 
 @Pipe({
-  name: 'AreaFilter'
+  name: 'areasFiltro'
 })
 export class AreasFiltroPipe implements PipeTransform {
 
-  transform(Area: Array<Area>, busqueda:string): Array<Area> {
-    const result:Array<Area> = [];
-    for(const post of Area){
-      if((post.nombre.toLocaleLowerCase().includes(busqueda.toLowerCase())) || (post.nombre.toUpperCase().includes(busqueda.toUpperCase()))){
-         result.push(post);
-      };
-    };
-    return result;
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
   }
 
 }
