75040f87b722ff9853c65fca9f28a5035891e2b9
CRIJOSAOR <88340482+CRIJOSAOR@users.noreply.github.com>
2022-11-09 11:26:38 -0600

    Revert "Merge branch 'master' into Alejandraf"
    This reverts commit 9daacd8c422c0f3513764bfd3c8f3129feb8cae2, reversing
    changes made to 02cf8c9662d8ead5acc944d4a60da4d8b6c35a4a.

--- POA/src/app/modules/gestion-pei-module/components-pei/objetivos/all-objetivo-component/all-objetivo-component.component.ts
+++ POA/src/app/modules/gestion-pei-module/components-pei/objetivos/all-objetivo-component/all-objetivo-component.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Dimension } from '../../../interfaces-pei/dimension.model';
 import { Objetivo } from '../../../interfaces-pei/objetivo.model';
 import { ObjetivosService } from '../../../services-pei/objetivos.service';
 
@@ -14,47 +13,34 @@ import { ObjetivosService } from '../../../services-pei/objetivos.service';
 export class AllObjetivoComponentComponent implements OnInit {
   errorMessage = '';
   objetivos: Objetivo | undefined;
-  //objetivo: Objetivo | undefined;
- 
 
   constructor( private _route: ActivatedRoute,private objetivosService:ObjetivosService,private router: Router,private toastr: ToastrService) { }
   idfun(){
     return this.id
   }
-  idDimensiones = ''
   public idDimension = Number(this._route.snapshot.paramMap.get('idDimension'));
   id = Number(this._route.snapshot.paramMap.get('id'));
   ngOnInit(): void {
-
-    this._route.paramMap.subscribe((params: ParamMap) => {
-      const idDimension = Number(this._route.snapshot.paramMap.get('idDimension'));
-      this.getObjetivoss(idDimension);
-      this.mostrarObjetivo(idDimension)
-      
-    })
-
-  
-    // const idDimension = Number(this._route.snapshot.paramMap.get('idDimension'));
+    const idDimension = Number(this._route.snapshot.paramMap.get('idDimension'));
     // console.log("aqui ")
     // console.log(id)
     // if (id) {
       // this.getObjetivoss(id);  
     // }
-    // console.log(this.dimensiones?.nombre)
-    // console.log(this.objetivos?.id)
-    //this.mostrarObjetivo(idDimension),
+    console.log(this.objetivos?.id)
+    this.mostrarObjetivo(idDimension),
     this.initData_Dimension();
     console.log(this.mostrarObjetivo);
     this.objetivosService.getObjetivos().subscribe((response:any) =>console.log(response))
 
     
-    // console.log("aqui ")
-    // console.log(idDimension)
-    // if (idDimension) {
-    //   this.getObjetivoss(idDimension);  
-    // }
+    console.log("aqui ")
+    console.log(idDimension)
+    if (idDimension) {
+      this.getObjetivoss(idDimension);  
+    }
     console.log("que onda",this.objetivos?.idDimension)
-    console.log("revisa aqui",this.objetivos?.idDimension)
+    console.log(this.objetivos?.nombre)
   }
 
   //filtro
@@ -81,9 +67,7 @@ export class AllObjetivoComponentComponent implements OnInit {
   //variables de llamado
   objetivosList: any = [];
   dimensionList: any = [];
-  Listdimension: any=[];
-  /// esta es la que ocupo
-  dimension_seleccionado:number=this.idDimension;
+  dimension_seleccionado:string="";
   /*
   metodos para mostrar
   */
@@ -91,11 +75,6 @@ export class AllObjetivoComponentComponent implements OnInit {
     this.objetivosService.mostrar_objetivo_id(id).subscribe((response:any) => 
     this.objetivosList = response);
     }
-  
-  mostrarDimension(id:number){
-    this.objetivosService.mostrar_objetivo_id(id).subscribe((response:any) => 
-    this.Listdimension = response);
-  }
     async initData_Dimension(){
 
       this.objetivosService.getdimensiones().subscribe((data:any) =>console.log(data));
@@ -103,10 +82,10 @@ export class AllObjetivoComponentComponent implements OnInit {
     }  
 
     //por id
-    mostrar_objetivos_idDimension(id: any):void {
-      this.objetivosService.mostrar_objetivo_id(id).subscribe((response:any) => 
+    mostrar_objetivos_idDimension(id: any) {
+      this.objetivosService.mostrar_objetivos_id(id).subscribe((response:any) => 
       this.objetivosList = response);
-      //this.router.navigate(['/gestion_pei/objetivos/list/',id]);
+        
     }
 
     getObjetivoss(id: number): void {
@@ -120,18 +99,11 @@ export class AllObjetivoComponentComponent implements OnInit {
     // }
 
     toDetail(id:number){
-      
       this.router.navigate(['/gestion_pei/objetivos/detail/',id.toString(),this.idDimension]);
     }
 
     toCreate(){
-      this._route.paramMap.subscribe((params: ParamMap) => {
-        const idDimension = Number(this._route.snapshot.paramMap.get('idDimension'));
-        //this.getObjetivoss(idDimension);
-        this.router.navigate(['/gestion_pei/objetivos/create/',idDimension]);
-        
-      })
-      
+      this.router.navigate(['/gestion_pei/objetivos/create/',this.idDimension.toString()]);
     }
     
   
