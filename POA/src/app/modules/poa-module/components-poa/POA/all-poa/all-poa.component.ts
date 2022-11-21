8f3a6ee487685d0ed59920d638d83e9b52759d27
LeticiaOchoa <88337103+LeticiaOchoa@users.noreply.github.com>
2022-11-21 05:17:58 -0600

    Cambios en POA

--- POA/src/app/modules/poa-module/components-poa/POA/all-poa/all-poa.component.ts
+++ POA/src/app/modules/poa-module/components-poa/POA/all-poa/all-poa.component.ts
@@ -23,7 +23,7 @@ export class AllPoaComponent implements OnInit {
     private _route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    
+
   }
 
   public idDepto: number = Number(this._route.snapshot.paramMap.get('idDepto'));
@@ -31,18 +31,18 @@ export class AllPoaComponent implements OnInit {
   rutaActual = "poa";
   public poa: Array<Poa> = [];
   public DeptoList: Array<Depto> = [];
-  public DeptoSeleccionado : number = this.idDepto;
+  public DeptoSeleccionado: number = this.idDepto;
   public user = this.Storage.get_storage("user"); // obtener el usuario logueado
   public filter: string = ""; // para filtar la tabla
   public _delete: string = ""; // define que elemento sera eliminado
   public data_update: Poa | any = this.poa_example; // define datos de un elemento a actualizar
   public poa_seleccionado: string = "";
 
-  public page:number=0;
-  public actualpage:number = 1;
-  public step:number=5;
-  public maxPages:number=1;
-  public enumPages:number[]=[]
+  public page: number = 0;
+  public actualpage: number = 1;
+  public step: number = 5;
+  public maxPages: number = 1;
+  public enumPages: number[] = []
 
   async initData() {
     this.poa = await firstValueFrom(this.service.MostrarPoa(this.idDepto));
@@ -51,7 +51,7 @@ export class AllPoaComponent implements OnInit {
     console.log(this.poa);
     this.maxPages = Math.round(this.poa.length / this.step);
     // sirve para generar los botones en paginacion
-    this.enumPages =  Array(this.maxPages).fill(null).map((x,i)=>i).slice(1,this.maxPages + 1) ;
+    this.enumPages = Array(this.maxPages).fill(null).map((x, i) => i).slice(1, this.maxPages + 1);
   }
 
   toDetail(idPoa: number) {
@@ -60,24 +60,24 @@ export class AllPoaComponent implements OnInit {
   toCreate() {
     this.router.navigate(['/gestion_poa/poa/create/', this.idDepto.toString()]);
   }
-  toHome(){
+  toHome() {
     this.router.navigate(['/home']);
   }
-  selectDepto(){
-    this.router.navigate(['/gestion_poa/poa/list/',this.DeptoSeleccionado]);
+  selectDepto() {
+    this.router.navigate(['/gestion_poa/poa/list/', this.DeptoSeleccionado]);
     setTimeout(function () {
       window.location.reload();
     }, 10)
   }
-  nextPage(){
+  nextPage() {
     this.page = this.page + this.step;
     this.actualpage++;
   }
-  previousPage(){
+  previousPage() {
     this.page = this.page - this.step;
     this.actualpage--;
   }
-  selectPage(numPage:number){
+  selectPage(numPage: number) {
     this.page = numPage * this.step;
   }
 
