8058529c962ef645411aea826b0a334ce48304e7
Brian Rodas <113316700+BrianRodas28200@users.noreply.github.com>
2022-12-03 00:01:14 -0600

    actualización

--- POA/src/app/modules/poa-module/components-poa/tareas/update-tareas/update-tareas.component.ts
+++ POA/src/app/modules/poa-module/components-poa/tareas/update-tareas/update-tareas.component.ts
@@ -13,61 +13,61 @@ import { TareasService } from '../../../services-poa/tareas.service';
 })
 export class UpdateTareasComponent implements OnInit {
   color: ThemePalette = 'primary';
-  isPresupueseto=true;
+  isPresupueseto = true;
   checked = false;
   disabled = false;
   selected = "4"
-  isPresupuesto:boolean=false;
-  
-  public unidadmedida_seleccionado:string="";
-  public objetogasto_seleccionado:string="";
-  public grupogasto_seleccionado:string="";
-  public fuente_seleccionado:string="";
-  
+  isPresupuesto: boolean = false;
+
+  public unidadmedida_seleccionado: string = "";
+  public objetogasto_seleccionado: string = "";
+  public grupogasto_seleccionado: string = "";
+  public fuente_seleccionado: string = "";
+
 
   id = Number(this.route.snapshot.paramMap.get('id'));
-  idActividad:number = Number(this.route.snapshot.paramMap.get('idActividad'));
-  idPresupuesto:number = Number(this.route.snapshot.paramMap.get('idPresupuesto'));
-  
-  public tareas:Tareas | any = {};
+  idActividad: number = Number(this.route.snapshot.paramMap.get('idActividad'));
+  idPresupuesto: number = Number(this.route.snapshot.paramMap.get('idPresupuesto'));
+
+  public tareas: Tareas | any = {};
   //variables 
-  public nombre:string='';
-  public descripcion:string='';
-  public costounitario:number=0
-  public cantidad:number=0
-  public total:number=0
-  public idobjeto:number=0
-  public idfuente:number=0
-  public idunidad:number=0
+  public nombre: string = '';
+  public descripcion: string = '';
+  public costounitario: number = 0
+  public cantidad: number = 0
+  public total: number = 0
+  public idobjeto: number = 0
+  public idfuente: number = 0
+  public idunidad: number = 0
 
 
 
-  constructor(private route: ActivatedRoute, 
-    private router: Router, 
-    private tareaservice:TareasService) { }
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    private tareaservice: TareasService) { }
 
   ngOnInit(): void {
     if (this.id) {
-      this.getTareass(this.id);  
+      this.getTareass(this.id);
     }
   }
 
   getTareass(id: number): void {
     this.tareaservice.getTareas(id).subscribe({
-      next: tareas => {this.tareas = tareas}
+      next: tareas => { this.tareas = tareas }
       // error: err => this.errorMessage = err
     });
   }
 
-  public tareass:Presupuesto | any = {};
-  List:Array<Tareas>=[];
-  mostrarObjeto(nombre:string): void {
+  public tareass: Presupuesto | any = {};
+  List: Array<Tareas> = [];
+  mostrarObjeto(nombre: string): void {
     this.tareaservice.Probando(nombre).subscribe({
-      next: tareas => {this.tareass = tareas}
+      next: tareas => { this.tareass = tareas }
       // error: err => this.errorMessage = err
     });
   }
-  Update():any{
+  Update(): any {
     let nombre = this.nombre;
     let descripcion = this.descripcion;
     let cantidad = this.cantidad;
@@ -76,49 +76,54 @@ export class UpdateTareasComponent implements OnInit {
     let idobjeto = this.idobjeto;
     let idfuente = this.idfuente;
     let idunidad = this.idunidad;
-    console.log(":"+nombre+":" + ":"+descripcion);
-     // validaciones
-    if((nombre === '')){nombre = this.tareas.nombre}
-    if((descripcion === '')){descripcion = this.tareas.descripcion}
-    if((cantidad==0)){cantidad = this.tareas.presupuesto.cantidad}
-    if((costounitario==0)){costounitario = this.tareas.presupuesto.costounitario}
-    if((total==0)){total = this.tareas.presupuesto.total}
-    if((idobjeto==0)){idobjeto = this.tareas.presupuesto.idobjeto}
-    if((idfuente==0)){idfuente = this.tareas.presupuesto.idfuente}
-    if((idunidad==0)){idunidad = this.tareas.presupuesto.idunidad}
-    console.log(":"+nombre+":" + ":"+descripcion);
-     try{
-      total=cantidad*costounitario
-      this.tareaservice.actualizarTarea(nombre,descripcion,this.id,this.isPresupueseto,this.idActividad,cantidad,this.idPresupuesto,costounitario,total,idobjeto,idfuente,idunidad).subscribe((res:any)=>{    
+    console.log(":" + nombre + ":" + ":" + descripcion);
+    // validaciones
+    if ((nombre === '')) { nombre = this.tareas.nombre }
+    if ((descripcion === '')) { descripcion = this.tareas.descripcion }
+    if ((cantidad == 0)) { cantidad = this.tareas.presupuesto.cantidad }
+    if ((costounitario == 0)) { costounitario = this.tareas.presupuesto.costounitario }
+    if ((total == 0)) { total = this.tareas.presupuesto.total }
+    if ((idobjeto == 0)) { idobjeto = this.tareas.presupuesto.idobjeto }
+    if ((idfuente == 0)) { idfuente = this.tareas.presupuesto.idfuente }
+    if ((idunidad == 0)) { idunidad = this.tareas.presupuesto.idunidad }
+    console.log(":" + nombre + ":" + ":" + descripcion);
+    try {
+      total = cantidad * costounitario
+      this.tareaservice.actualizarTarea(nombre, descripcion, this.id, this.isPresupueseto, this.idActividad, cantidad, this.idPresupuesto, costounitario, total, idobjeto, idfuente, idunidad).subscribe((res: any) => {
         Swal.fire({
+          icon: 'success',
+          title: '¡Actualizado con éxito!',
+          showConfirmButton: false,
+          timer: 2500
+        })
+      }, (error: any) => {
+
+      });
+      this.onBack()
+
+    } catch (error) {
+      console.log(error);
+      Swal.fire({
         icon: 'success',
-        title: '¡Actualizado con éxito!',
+        title: 'Ha ocurrido un error',
         showConfirmButton: false,
-        timer: 2500
+        timer: 2500,
       })
-     },(error:any)=>{
-      
-     });
-this.onBack()
+      this.toDetail();
 
-} catch(error){
-  console.log(error);
-  Swal.fire({
-   icon: 'success',
-   title: 'Ha ocurrido un error',
-   showConfirmButton: false,
-   timer: 2500,
- })
- this.onBack()
-}
+    }
 
-}
+  }
+  
 
-onBack(): void {
-  this.router.navigate(['/gestion_poa/tareas/detail/',this.idActividad]);
-}
-toDetail(){
-  this.router.navigate(['/gestion_poa/tareas/detail/',this.id,this.idActividad]);
-}
+  onBack(): void {
+    this.router.navigate(['/gestion_poa/tareas/detail/', this.id, this.idActividad]);
+  }
+  toDetail() {
+    this.router.navigate(['/gestion_poa/tareas/detail/', this.id, this.idActividad])
+    setTimeout(function () {
+      window.location.reload();
+    }, 10)
+  }
 }
 
