96b72e3225abf49cf131a83b1412b095af982a35
David Mendoza <david.mendoza@unah.hn>
2022-12-07 16:30:23 -0600

    creados los modulos reportes y revision

---
+++ POA/src/app/modules/reportes-module/reportes-module.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reportes-module',
+  templateUrl: './reportes-module.component.html',
+  styleUrls: ['./reportes-module.component.css']
+})
+export class ReportesModuleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ POA/src/app/modules/reportes-module/reportes-module.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReportesModuleComponent } from './reportes-module.component';
+
+
+
+@NgModule({
+  declarations: [
+    ReportesModuleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ReportesModuleModule { }
