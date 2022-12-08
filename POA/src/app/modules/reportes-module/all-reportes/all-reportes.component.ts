64bcbde1ca7e60a8809a408a548852986756fd16
jaimeluna24 <jaimeluna600@gmail.com>
2022-12-07 22:36:10 -0600

    Creacion de la pantalla de reportes y estadisticos

---
+++ POA/src/app/modules/reportes-module/all-reportes/all-reportes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-reportes',
+  templateUrl: './all-reportes.component.html',
+  styleUrls: ['./all-reportes.component.css']
+})
+export class AllReportesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- POA/src/app/modules/reportes-module/reportes-module.module.ts
+++ POA/src/app/modules/reportes-module/reportes-module.module.ts
@@ -3,17 +3,33 @@ import { CommonModule } from '@angular/common';
 import { ReportesModuleComponent } from './reportes-module.component';
 import { RouterModule } from '@angular/router';
 
+import { TopBarComponent } from 'src/app/_core/top-bar/top-bar.component';
+import { AllReportesComponent } from './all-reportes/all-reportes.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BackButtonComponent } from 'src/app/_core/back-button/back-button.component';
+import {MatButtonModule} from '@angular/material/button';
+
 const router = RouterModule.forChild([
-  {path: '', component: ReportesModuleComponent}
+  {path: '', component: ReportesModuleComponent},
+
+  {path: 'list', component: AllReportesComponent},
 ])
 
 @NgModule({
   declarations: [
-    ReportesModuleComponent
+    ReportesModuleComponent,
+    AllReportesComponent
+
   ],
   imports: [
     CommonModule,
-    router
+    router,
+    TopBarComponent,
+    MatCardModule,
+    MatTabsModule,
+    BackButtonComponent,
+    MatButtonModule
   ]
 })
 export class ReportesModuleModule { }
