bf1faced725bedebc1cd9028bad6dd8878e7be6f
Brian Rodas <113316700+BrianRodas28200@users.noreply.github.com>
2022-12-09 21:11:31 -0600

    Pantalla principal de revision

--- POA/src/app/modules/revision-module/components/revisar-tareas/revisar-tareas.component.ts
+++ POA/src/app/modules/revision-module/components/revisar-tareas/revisar-tareas.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+import { PoaService } from 'src/app/modules/poa-module/services-poa/poa.service';
+import { Storage } from 'src/app/_core/global-services/local_storage.service';
+
 
 @Component({
   selector: 'app-revisar-tareas',
@@ -7,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RevisarTareasComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Storage: Storage,
+    private service: PoaService,
+    private router: Router,
+    private _route: ActivatedRoute
+    ) { }
 
   ngOnInit(): void {
+
   }
+
 
 }
