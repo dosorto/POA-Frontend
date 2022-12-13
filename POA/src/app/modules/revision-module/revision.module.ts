040c4d57f462fbb737339e80955a5d2f83948a10
Brian Rodas <113316700+BrianRodas28200@users.noreply.github.com>
2022-12-12 19:38:32 -0600

    Se agregaron funciones en "revisar-tareas"

--- POA/src/app/modules/revision-module/revision.module.ts
+++ POA/src/app/modules/revision-module/revision.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RevisionComponent } from './revision.component';
 import { RouterModule } from '@angular/router';
+import { TopBarComponent } from '../../_core/top-bar/top-bar.component';
 import { RevisarTareasComponent } from './components/revisar-tareas/revisar-tareas.component';
 import { TareasRevisadasComponent } from './components/tareas-revisadas/tareas-revisadas.component';
 import { TareasAprobadasComponent } from './components/tareas-aprobadas/tareas-aprobadas.component';
 import { TareasRechazadasComponent } from './components/tareas-rechazadas/tareas-rechazadas.component';
+import { BackButtonComponent } from 'src/app/_core/back-button/back-button.component';
 
 const router = RouterModule.forChild([
   {path: '', component: RevisionComponent},
@@ -27,7 +29,9 @@ const router = RouterModule.forChild([
   ],
   imports: [
     CommonModule,
-    router
+    router,
+    TopBarComponent,
+    BackButtonComponent
   ]
 })
 export class RevisionModule { }
